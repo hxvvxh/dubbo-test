46ca5075d6393a7f2bffcb9f98e325eeeaa45b19
hxvvxh <2732259311@qq.com>
2020-09-09 22:16:27 +0800

    commit push branch  of version 1

--- commit.sh
+++ commit.sh
@@ -2,9 +2,9 @@
 
 set -x
 
-version=`cd $project_loc && mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
-
 branch= git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+
+version=`cd $project_loc && mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
 
 git add .
 
